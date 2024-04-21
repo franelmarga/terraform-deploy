39293dd0d927d6c40ae9ec1bb74d1e3d41dfb374
franmarga <francesc.margarit@build38.com>
2024-04-21 14:20:41 +0200

    removed AZ dependency

--- main.tf
+++ main.tf
@@ -2,15 +2,14 @@
 # We can define the variables in the terraform.tfvars file and then use them in the main.tf file, for conserving the granularity of the configuration.
 
 module "infra-network" {
-  source = "git::https://github.com/franelmarga/terraform-network-module.git?ref=1.0.1"
-
-  env                = var.env
-  region             = var.region
-  vpc_cidr           = var.vpc_cidr
-  availability_zones = var.availability_zones
-  public_subnets     = var.public_subnets
-  private_subnets    = var.private_subnets
-  create_nat_gateway = var.create_nat_gateway
-  repo_tags          = var.repo_tags
-  env_tags           = var.env_tags
+  source                = "git::https://github.com/franelmarga/terraform-network-module.git?ref=1.0.4"
+  env                   = var.env
+  region                = var.region
+  vpc_cidr              = var.vpc_cidr
+  public_subnets_cidrs  = var.public_subnets_cidrs
+  private_subnets_cidrs = var.private_subnets_cidrs
+  create_nat_gateway    = var.create_nat_gateway
+  repo_tags             = var.repo_tags
+  env_tags              = var.env_tags
 }
+
